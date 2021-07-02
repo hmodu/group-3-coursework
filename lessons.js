100a2edb3c6830d22db723922b701f56a99cd774
Modu <35815941+hmodu@users.noreply.github.com>
2021-07-02 19:41:37 +0100

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,37 @@
+const LESSONS = [
+  {
+    name: "Biology",
+    location: "Birmingham",
+    price: 1500,
+    spaces: 5,
+    image: "./images/biology.webp",
+  },
+  {
+    name: "Chemistry",
+    location: "Coventry",
+    price: 2500,
+    spaces: 5,
+    image: "./images/chemistry.jpg",
+  },
+  {
+    name: "Deno",
+    location: "Delta",
+    price: 3500,
+    spaces: 5,
+    image: "./images/deno.png",
+  },
+  {
+    name: "Endocrinology",
+    location: "Egbeda",
+    price: 4500,
+    spaces: 5,
+    image: "./images/endo.jpg",
+  },
+  {
+    name: "Fine Arts",
+    location: "Finland",
+    price: 5500,
+    spaces: 5,
+    image: "./images/finarts.png",
+  },
+];
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+var cacheName = "petstore-v2";
+var cacheFiles = [
+  "index.html",
+  "petstore.webmanifest",
+  "images/512.png",
+  "images/biology.webp",
+  "images/chemistry.jpg",
+  "images/deno.png",
+  "images/endo.jpg",
+  "images/finarts.png",
+  "styles.css",
+  "lessons.js",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      // Download the file if it is not in the cache,
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          // add the new file to cache
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
